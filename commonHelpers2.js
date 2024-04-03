0ed023d9c24cc061f96ff48908e3e0b2fc633cd7
Voytina <142828618+Voytina@users.noreply.github.com>
2024-04-03 05:42:44 +0000

    Deploying to gh-pages from @ Voytina/goit-js-hw-10@c3c90f6350f3f52a1b1b1038fdbcccd6726c081a 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i as o}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",r=>{r.preventDefault();const t=m.elements.delay.value,s=r.currentTarget.querySelector('input[name="state"]:checked');let i;s&&(i=s.value),new Promise((e,l)=>{setTimeout(()=>{i==="fulfilled"?e(t):l(t)},t)}).then(e=>{o.success({title:"Caution",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{o.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
